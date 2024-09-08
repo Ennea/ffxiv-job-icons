f1924f9516c1f3ae28482541b2b5c450b39aa2e0
Ennea <Ennea@users.noreply.github.com>
2024-09-08 10:48:46 +0200

    add build script and all necessities for it; also README, .gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/xivapi/classjob-icons
---
+++ build.fish
@@ -0,0 +1,42 @@
+#!/usr/bin/fish
+
+if not set -q CONSOLE_TOOLS_CMD
+    echo "Environment variable CONSOLE_TOOLS_CMD must be set"
+    exit
+end
+
+
+# create build directory and cd into it
+if not test -d build
+    mkdir build
+end
+cd build
+
+# convert SVGs to PNG using inkscape
+echo -n 'Making PNGs '
+for file in (find ../isvg/ -type f -regex '.*?/062.*svg')
+    echo -n .
+    set -l base_name (string replace '.svg' '' (string split '/' $file)[-1])
+    inkscape -o "$base_name".png -C -w 32 -h 32 $file
+    inkscape -o "$base_name"_hr1.png -C -w 64 -h 64 $file
+end
+echo
+
+# convert PNGs to TEX using TexTools' ConsoleTools
+set -l cmd (string split ' ' $CONSOLE_TOOLS_CMD)
+echo -n 'Making TEXs '
+for file in (ls *.png)
+    echo -n .
+    set -l out_name (string replace '.png' '' $file).tex
+    WINEDEBUG=-all $cmd '/wrap' $file $out_name ui/icon/062000/$out_name > /dev/null
+end
+echo
+
+echo 'Removing PNGs'
+rm *.png
+
+# create the PMP file
+echo 'Making PMP'
+cp -r ../mod_template/* .
+mv *.tex ui/icon/062000
+zip -r role-colored-glowing-job-icons.pmp *
